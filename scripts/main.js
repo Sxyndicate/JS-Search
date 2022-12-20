a867e6bc099b58351e1b052fcc76c1e3fdd183ee
Sxyndicate <99019754+Sxyndicate@users.noreply.github.com>
2022-12-20 20:01:07 +0200

    1.1fail

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+function search() {
+    let filter = document.getElementById('find').value.toUpperCase();
+
+    let item = document.querySelectorAll('.product');
+
+    let l = document.getElementsByTagName('h3');
+
+    for(var i = 0;i<=l.length;i++){
+        let a=item[i].getElementsByTagName('h3')[0];
+
+        let value=a.innerHTML || a.innerText || a.textContent;
+
+        if(value.toUpperCase().indexOf(filter) > -1) {
+            item[i].getElementsByClassName.display="";
+        }
+        else{
+            item[i].getElementsByClassName.display="none";
+        }
+    }
+}
